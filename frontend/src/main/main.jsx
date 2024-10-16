aa220d1eb34731c6e8e924499a230b9508a2f51f
윤민성 <dbsalstj2310@gmail.com>
2024-10-16 20:30:45 +0900

    feat: login, createPost, main  test

--- frontend/src/main/main.jsx
+++ frontend/src/main/main.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Main() {
     return (
       <div>
-        <h2>Main</h2>
-        <h3>게시판이 들어올 것</h3>
+        <h1>
+          <Link to = "/main">메인 페이지</Link>
+        </h1>
+        <div>
+          <Link to = "/create_post" className = "link">
+          게시물 추가
+          </Link>
+        </div>
       </div>
     );
   }
